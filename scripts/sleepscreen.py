65f4c23b036a6f958fafa0f447a1f432a3cff153
Qntn <62796186+qtaped@users.noreply.github.com>
2022-08-03 14:59:59 +0200

    using pynput to wake up

--- scripts/sleepscreen.py
+++ scripts/sleepscreen.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-
+# pip install pynput
 import RPi.GPIO as GPIO
+from pynput.keyboard import Key, Listener
 
 Num = 8
 
@@ -9,10 +10,14 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(Num, GPIO.OUT)
 
-
 state = GPIO.input(Num)
 
 if state:
     GPIO.output(Num, False)
-    input("Press enter to turn on screen.")
+
+def press(key):
     GPIO.output(Num, True)
+    exit()
+with Listener(on_press = press) as listener:
+    listener.join()
+
