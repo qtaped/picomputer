652de021d48567d91011b07b6ef0754d5268c642
Qntn <62796186+qtaped@users.noreply.github.com>
2022-08-28 12:18:15 +0200

    ifconfig up/down, edit wpa

--- scripts/wifictl.sh
+++ scripts/wifictl.sh
@@ -18,50 +18,45 @@ reload_interface
 
 while true; do
 
-echo -e "\e[1;44m s\e[0mcan,  \e[1;42m c\e[0monnect, \e[1;41m d\e[0misconnect, \e[1;43m o\e[0mther? or (q)uit."
+echo -e "\e[1;44m s\e[0mcan,  \e[1;42m c\e[0monnect, \e[1;41m d\e[0misconnect, \e[1;40m e\e[0mdit, \e[1;43m o\e[0mther? or (q)uit."
 echo -e "\e[1;30;47m"
 read -p "Enter your choice:" -n 1 action
 echo -e "\e[0m"
 
    case $action in
 
-[s]* ) 
-        reload_interface
-        echo -n "Scanning... "
-        iwctl station $wifi_card scan on
-        sleep 2
-        reload_interface
-        echo -e "\e[94mScan on.\e[0m\n";;
-[S]* ) 
-        iwctl station $wifi_card scan off
-        reload_interface
-        echo -e "\e[91mScan off.\e[0m\n";;
-
-[cC]* ) 
+[s]* )
        reload_interface
-       echo "(Leave blank and hit return to cancel.)"
-       read -p "Enter SSID: " ssid 
-if [ -z $ssid ]; then
-reload_interface
-echo -e "Canceled.\n"
-else
-       iwctl station $wifi_card connect $ssid
+       echo -n "Scanning... "
+       iwctl station $wifi_card scan on
+       sleep 2
        reload_interface
-       echo -e "\e[92mReaching $ssid\e[0m\n"
-fi;;   
-[dD]* ) 
-        reload_interface
-        iwctl station $wifi_card disconnect
-        echo -e "\e[91m[$wifi_card]\tDisconnected.\e[0m\n";;
+       echo -e "\e[94mScanned.\e[0m\n";;
+[S]* )
+       iwctl station $wifi_card scan off
+       reload_interface
+       echo -e "\e[91mScan off.\e[0m\n";;
+[cC]* )
+       sudo ifconfig $wifi_card up
+       sleep 2
+       reload_interface
+       echo -e "\e[92m$wifi_card up\e[0m\n";;
+[dD]* )
+       sudo ifconfig $wifi_card down
+       reload_interface
+       echo -e "\e[91m$wifi_card down\e[0m\n";;
+[eE]* )
+       sudo editor /etc/wpa_supplicant/wpa_supplicant.conf
+       reload_interface;;
 
-[oO]* ) 
-        iwctl
-        reload_interface;;
+[oO]* )
+       iwctl
+       reload_interface;;
 
 [qQ]* ) exit;;
 
-* )     reload_interface
-        echo -e "\e[91mWrong choice.\e[0m\n";;
+* )    reload_interface
+       echo -e "\e[91mWrong choice.\e[0m\n";;
 
    esac
 
